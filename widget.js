eab733c8c683a8d26a1af328cc47e3930cf6c79a
FinnPegler <finnpegler@gmail.com>
2020-01-29 13:07:42 +0000

    First session of implementing widget

--- widget.js
+++ widget.js
@@ -1,1 +1,12 @@
-let count6 = 0;
+document.getElementById("element1").addEventListener("click", hide)
+document.getElementById("exitbtn").addEventListener(click, hide2)
+
+
+function hide () {
+    document.getElementById("element1").className = "hidden";
+}
+
+function hide2 () {
+    document.getElementById("element2").className = "hidden";
+}
+
