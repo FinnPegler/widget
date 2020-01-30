1620bfa0fa84ccc2f39c5e41666e17514af803f7
FinnPegler <finnpegler@gmail.com>
2020-01-30 12:49:39 +0000

    working on widget

--- widget.js
+++ widget.js
@@ -1,12 +1,17 @@
-document.getElementById("element1").addEventListener("click", hide)
-document.getElementById("exitbtn").addEventListener(click, hide2)
-
+document.getElementById("element1").addEventListener("click", hide);
+document.getElementById("close").addEventListener('click', hide2);
+document.getElementById("minimise").addEventListener('click', minimise);
 
 function hide () {
     document.getElementById("element1").className = "hidden";
+    document.getElementById("element2").className = "visible";
 }
 
 function hide2 () {
     document.getElementById("element2").className = "hidden";
 }
 
+function minimise () {
+    document.getElementById("element2").className = "hidden";
+    document.getElementById("element1").className = "visible";
+}
